fc8ae55c9ed515baace0b5fc3fddae8cc16860bb
Andreas Landerer <andreas.landerer@gmail.com>
2018-01-11 09:16:58 +0100

    Added new controller so that instances of this controller can be retrieved from the application context

--- src/main/java/com/landerer/spring/didemo/MainApplication.java
+++ src/main/java/com/landerer/spring/didemo/MainApplication.java
@@ -1,11 +1,15 @@
 package com.landerer.spring.didemo;
 
+import com.landerer.spring.didemo.controllers.DiDemoController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        final ApplicationContext context = SpringApplication.run(MainApplication.class, args);
+        final DiDemoController controller = (DiDemoController) context.getBean("diDemoController");
+        System.out.println(controller.sayHello());
     }
 }
