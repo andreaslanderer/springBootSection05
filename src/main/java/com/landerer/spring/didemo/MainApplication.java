94a98aaa6cf75949ff2ea44c0f0e11cfe5855366
Andreas Landerer <andreaslanderer@users.noreply.github.com>
2018-01-11 15:11:39 +0100

    Added new Service with external Java Configuration (#2)

--- src/main/java/com/landerer/spring/didemo/MainApplication.java
+++ src/main/java/com/landerer/spring/didemo/MainApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import services.JokeService;
 
 // If no @ComponentScan is provided Spring will scan this package and all subpackages
 @SpringBootApplication
-@ComponentScan(basePackages = {"services", "controllers"})
+@ComponentScan(basePackages = {"services", "controllers", "com.landerer.spring.didemo.config"})
 public class MainApplication {
     public static void main(String[] args) {
         final ApplicationContext context = SpringApplication.run(MainApplication.class, args);
@@ -20,5 +21,9 @@ public class MainApplication {
         System.out.println(context.getBean(SetterInjectedController.class).sayHello());
         System.out.println(context.getBean(ConstructorInjectedController.class).sayHello());
         System.out.println(context.getBean(PrimaryController.class).sayHello());
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(context.getBean(JokeService.class).getJoke());
+        }
     }
 }
---
+++ src/main/java/services/JokeServiceImpl.java
@@ -0,0 +1,21 @@
+package services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes quotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes quotes) {
+        this.quotes = quotes;
+    }
+
+    @Override
+    public String getJoke() {
+        return this.quotes.getRandomQuote();
+    }
+}
